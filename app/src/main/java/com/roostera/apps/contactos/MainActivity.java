9a3eaf862edf936ad99f879424ea261ea3d02dd8
javiercruzweb <javiercruzweb@gmail.com>
2015-08-18 14:10:12 -0500

    first commit

--- app/src/main/java/com/roostera/apps/contactos/MainActivity.java
+++ app/src/main/java/com/roostera/apps/contactos/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
+    final static String nivel = "0";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
